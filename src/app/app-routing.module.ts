8737ca0fc468c4da868e6b663d9b89dcd4a26606
Ayush Shrestha <ayush@shresthabros.com>
2023-07-30 16:55:15 +0545

    login/signup/forgotpassword page created and implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { IndexComponent } from "./shared/index/index.component";
+import { SignupComponent } from "./pages/signup/signup.component";
+import { LoginComponent } from "./pages/login/login.component";
+import { ForgotpasswordComponent } from "./pages/forgotpassword/forgotpassword.component";
+import { NotFoundComponent } from "./shared/not-found/not-found.component";
+
+const routes: Routes = [
+  //{ path: '', redirectTo: '/login', pathMatch: 'full'},
+  { path: '', component: IndexComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'forgotpassword', component: ForgotpasswordComponent },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+
 import { HeaderComponent } from './shared/header/header.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
+
+import { IndexComponent } from './shared/index/index.component';
+
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { HighchartsChartModule } from 'highcharts-angular';
 import { Select2Module } from 'ng-select2-component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ForgotpasswordComponent } from './pages/forgotpassword/forgotpassword.component';
+import { NotFoundComponent } from './shared/not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     SidebarComponent,
-    DashboardComponent
+    IndexComponent,
+    DashboardComponent,
+    SignupComponent,
+    LoginComponent,
+    ForgotpasswordComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
